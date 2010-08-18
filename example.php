23e047e26087ad53c51246b9e5109409b878f2ae
Tim Whitlock <r-github@whiteinteractive.com>
2010-08-18 18:44:43 +0100

    initial add

---
+++ example.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * 
+ */
+error_reporting( E_ALL | E_STRICT );
+require 'VarnishAdminSocket.php';
+
+
+
+$Sock = new VarnishAdminSocket( 'localhost', 6082 );
+
+$banner = $Sock->connect();
+echo $banner, "\n\n";
+
+echo "Purge list\n";
+$list = $Sock->purge_list();
+var_dump( $list );
+
+
+$Sock->quit();
+
+
+
+
+
+
+
+
+
